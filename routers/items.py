5281e428f50dbace4f2fc74016ace77a25b7e578
laksh <lakshyam38@gmail.com>
2022-07-10 12:45:14 +0530

    JWT token verification on protected route

--- routers/items.py
+++ routers/items.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from . import auth
+from utils.token_utils import decode_access_token
+from utils.users_utils import get_user
 
 router = APIRouter()
 
@@ -9,4 +11,9 @@ items = ["apple", "mango", "banana"]
 
 @router.get("/")
 def read_items(token: str = Depends(auth.oauth2_scheme)):
+    token_data = decode_access_token(token)
+
+    if not get_user(token_data.username, auth.users_db):
+        raise HTTPException(status_code=401, detail="Not authorized")
+
     return {"results": items}
