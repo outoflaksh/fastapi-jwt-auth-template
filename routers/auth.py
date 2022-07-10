63d4b51359224626556bca9cbb9c25eb3eca1924
laksh <lakshyam38@gmail.com>
2022-07-10 11:17:11 +0530

    User verification from the db completed

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from passlib.context import CryptContext
 
 from models import User, UserInDB, SignupForm
 
@@ -10,21 +12,61 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-users = []
+users_db = [
+    {
+        "username": "user1",
+        "email": "user@email.com",
+        "name": "user1",
+        "hashed_password": "$2b$12$bTuZG0mNK2ciF1U2viC1YOuiV6cG2FCIzd..wXMBfpqnQCYbzlm6q",
+    }
+]
 
 
 @router.post("/register")
 def create_user(form_data: SignupForm = Depends()):
-    users.append(dict(form_data))
-    print(users)
+
     return {"msg": "OK"}
+
+
+# Utils
+
+
+def hash_password(password: str):
+    passwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    return passwd_context.hash(password)
+
+
+def verify_hash(password_input, hashed_password):
+    passwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    return passwd_context.verify(password_input, hashed_password)
+
+
+def get_user(username: str, db):
+    for user in db:
+        if user["username"] == username:
+            return UserInDB(**user)
+    return None
+
+
+def verify_user(form_data):
+    user = get_user(form_data.username, users_db)
+
+    if user:
+        password_input = form_data.password
+        hashed_password = user.hashed_password
+
+        if verify_hash(password_input, hashed_password):
+            return user
 
 
 @router.post("/login")
 def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
-    return {"access_token": form_data.username, "token_type": "bearer"}
+    user = verify_user(form_data)
+    print(hash_password(form_data.password))
 
+    if user is None:
+        raise HTTPException(status_code=400, detail="Invalid username or password!")
 
-@router.get("/protected")
-def protected_route(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+    return {"access_token": user.username, "token_type": "bearer"}
