c7d8bc55b7cdd96cb7d326584f7638373f1f1cad
laksh <lakshyam38@gmail.com>
2022-07-10 12:34:15 +0530

    Created function for decoding the JWT token

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
 
 
 class User(BaseModel):
--- utils/token_utils.py
+++ utils/token_utils.py
@@ -2,10 +2,13 @@ import os
 from datetime import datetime, time, timedelta
 from typing import Union
 from jose import jwt, JWTError
+from fastapi import HTTPException
 
 
 # Env vars
 from dotenv import load_dotenv
+
+from models import TokenData
 
 load_dotenv()
 
@@ -27,3 +30,19 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     access_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return access_token
+
+
+def decode_access_token(token: str):
+    credential_exception = HTTPException(status_code=401, detail="Invalid credentials!")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        username = payload.get("sub")
+        if username is None:
+            raise credential_exception
+
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+
+    return token_data
